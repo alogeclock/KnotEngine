69526fd04e5b544897e363f33509958c77bcf73e
alogeclock <alogeclock@gmail.com>
2026-09-10 09:26:11 +0900

    Feat: 화면 최대화 상태에서 엔진 실행

--- KnotEngine/Source/Engine/Platform/WindowsApplication.cpp
+++ KnotEngine/Source/Engine/Platform/WindowsApplication.cpp
@@ -116,12 +116,12 @@ void FWindowsApplication::Startup(HINSTANCE InInstance, int ShowCmd)
 
 	panicf(RegisterClassExW(&WindowClass), "RegisterClassExW 실패. GetLastError()={}", GetLastError());
 
-	HWND WindowHandle = CreateWindowExW(0, ClassName, Title, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, nullptr, nullptr, Instance, this);
+	HWND WindowHandle = CreateWindowExW( 0, ClassName, Title, WS_MAXIMIZE | WS_VISIBLE | WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, nullptr, nullptr, Instance, this);
 	panicf(WindowHandle, "CreateWindowExW 실패. GetLastError()={}", GetLastError());
 
 	Window.Startup(WindowHandle);
 	WindowsInput.Startup(WindowHandle);
-	ShowWindow(WindowHandle, ShowCmd);
+	ShowWindow(WindowHandle, SW_SHOWMAXIMIZED);
 	UpdateWindow(WindowHandle);
 }
 
